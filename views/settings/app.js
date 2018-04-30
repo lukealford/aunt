76cef0be3aafc7120d4081a058d55adc51083915
Luke Alford <luke@lukealford.com>
2018-05-01 00:27:26 +1000

    Fixed 0 index month breaking avg and days to rollover

--- views/settings/app.js
+++ views/settings/app.js
@@ -66,7 +66,7 @@ function showData(arg) {
     console.log('data in bytes', arg.usage.left1[0]);
     
     avgUse = planInBytes / getDaysTillRoll(arg.usage.rollover[0]);
-    console.log(avgUse);
+    console.log('avg daily',avgUse);
     if (arg.usage.allowance1_mb > 1000000 && arg.usage.allowance1_mb < 99999999) {
         let plan = arg.usage.allowance1_mb / 1000000
         content = "<p><span>Download:</span><span class='string'>" + main.formatFileSize(arg.usage.down1[0]) + "</span></p><p><span>Upload:</span><span class='string'>" + main.formatFileSize(arg.usage.up1[0]) + "</span></p><p>" + "<span>Total Left:</span><span class='string'>" + main.formatFileSize(arg.usage.left1[0]) + "<span>/</span>" + plan + " TB" + "</span></p><p>" + "<span>Updated:</span><span class='string'>" + moment(arg.usage.lastupdated[0]).fromNow() + "</span></p><p>" + "<span>Days in month remaining:</span><span class='string'>" + getDaysTillRoll(arg.usage.rollover[0]) + " day/s<span></p><p><span>Avg daily limit:</span><span class='string'>" + main.formatFileSize(avgUse) + "</span></p>";
@@ -90,15 +90,16 @@ function showData(arg) {
 function getDaysTillRoll(day) {
     const date = new Date();
     const today = moment(date);
-
+    const  month = today.month();
     if (day < 10) {
         let rd = 0 + '' + day;
-        rd = moment(new Date(date.getFullYear(), date.getMonth() + 1, rd));
+
+        rd = moment(new Date(date.getFullYear(),month, rd));
         let d = rd.diff(today, 'days');
         return d;
     } else {
         let rd = day;
-        rd = moment(new Date(date.getFullYear(), date.getMonth() + 1, rd));
+        rd = moment(new Date(date.getFullYear(), month, rd));
         let d = rd.diff(today, 'days');
         return d;
     }
